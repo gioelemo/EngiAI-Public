a78a2728c59f0ccb7c5f5a53a0aad2feab5d5210
Gioele Molinari <gioelemo@ethz.ch>
2026-01-04 13:18:23 +0100

    Simplify scores

--- benchmarks/shared/__init__.py
+++ benchmarks/shared/__init__.py
@@ -1,12 +1,7 @@
 """Shared utilities for benchmark evaluations across different problem types."""
 
 from benchmarks.shared.scorers import (
-    score_constraint_accuracy,
     score_design_match,
-    score_no_contradictions,
-    score_provides_actionable_guidance,
-    score_target_awareness,
-    score_understands_tradeoffs,
 )
 from benchmarks.shared.utils import (
     create_design_comparison,
@@ -16,10 +11,5 @@ from benchmarks.shared.utils import (
 __all__ = [
     "create_design_comparison",
     "extract_design_from_tool_messages",
-    "score_constraint_accuracy",
     "score_design_match",
-    "score_no_contradictions",
-    "score_provides_actionable_guidance",
-    "score_target_awareness",
-    "score_understands_tradeoffs",
 ]
